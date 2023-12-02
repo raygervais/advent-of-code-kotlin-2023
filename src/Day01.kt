191f87a88b16a2cca592dc8d44bfaf1df6ba663b
raygervais <raygervais@hotmail.ca>
2023-12-02 17:40:19 -0500

    adds(D1): final implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { s -> "${s.first { c -> c.isDigit() }}${s.last { c -> c.isDigit() }}" }
+            .sumOf { s -> s.toInt() }
     }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val conversion = mapOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9",
+        )
+
+        val cleanup = fun(s: String): String {
+            var holder: String = ""
+            for (c in s) {
+                holder += c.toString()
+                for ((k, v) in conversion) {
+                    holder = holder.replace(k, v, true)
+                }
+            }
+            return holder
+        }
+
+        return input
+            .map { s -> cleanup(s) }
+            .map { s -> "${s.first { c -> c.isDigit() }}${s.last { c -> c.isDigit() }}" }
+            .sumOf { s -> s.toInt() }
     }
 
+
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_Test")
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_Test_2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
